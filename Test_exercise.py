d12a05858cf8472364facb180e59575050ddff00
Mysiuk12@gmail.com <Mysiuk12@gmail.com>
2022-11-26 22:29:08 +0100

    initial changes

---
+++ Test_exercise.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+class TitleFetch(unittest.TestCase):
+
+    def setUp(self) -> None:
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service)
+        self.driver.maximize_window()
+
+    def test_1(self):
+        self.driver.get("https://news.ycombinator.com/")
+        searchItems = self.driver.find_elements(By.XPATH, "//span[@class='titleline']/a")
+        for i in searchItems:
+            print(i.text)
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+if __name__ == '__main':
+    unittest.main()
